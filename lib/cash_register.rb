3e93188f399e7f19371476a06b94674e30d02187
sami suliman <sami.bco@gmail.com>
2019-08-28 01:27:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,11 +3,10 @@ class CashRegister
 
     attr_accessor :total, :discount, :cart, :last_transaction
     
-    @@item = []
-    
     def initialize(discount = 0)
         @total = 0
         @discount = discount
+        @cart = []
     end 
      
     def add_item(title, price, quantity = 1)
